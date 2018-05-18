e2fc4a6572b9f128b9140c70e5a9bc0c11c78f90
marietta25 <32923340+marietta25@users.noreply.github.com>
2018-05-18 09:19:13 +0300

    Added travis

--- test/e2e/ArthurTohver_Login_Twitter.js
+++ test/e2e/ArthurTohver_Login_Twitter.js
@@ -1,36 +1,37 @@
 var config = require('../../nightwatch.conf.js');
 
-module.exports = { 
-  '@tags': ['BoyishGiggles'],
-  'Twitter Login :)': function(browser) {
-    browser
-      .url('http://boyishgiggles.com/')
-      .waitForElementVisible('body')
-      .pause(1000)
-      .assert.containsText('#transparent','I want to be completely transparent...')
-      .saveScreenshot(config.imgpath(browser) + 'BoyishGiggles.png')
-      .pause(1000)
-      .waitForElementVisible('twitterwidget#twitter-widget-0',1000)
-      .pause(500)
-      .click('twitterwidget#twitter-widget-0')
-      .moveToElement('twitterwidget#twitter-widget-0', 0, 0)
-      .mouseButtonClick(0)
-      .pause(2500)
-      .waitForElementVisible('a[class="LoginDialog-signupLink"]',2000)
-      .pause(2500)
-      .click('a[class="LoginDialog-signupLink"]')
-      .pause(500)
-      .waitForElementVisible('a[id="signin-link"]',10)
-      .pause(1000)
-      
-      //.assert.containsText('DisRespect')
-      .pause(500)
-      .setValue('input[session[username_or_email]]', 'boyishgiggles')
-      .pause(500)
-      .setValue('input[session[password]]')
-      .pause(500)
-      .saveScreenshot(config.imgpath(browser) + 'logindata.png')
-      .pause(1000)
-      .end();
-  }
+module.exports = {
+    '@tags': ['BoyishGiggles'],
+    before: function(browser) {
+        browser.resizeWindow(1300, 800);
+    },
+    'Twitter Login :)': function(browser) {
+        browser
+            .url('https://twitter.com/')
+
+        .waitForElementVisible('body')
+            .pause(1000)
+            .assert.urlContains('twitter')
+            .saveScreenshot(config.imgpath(browser) + 'twitterstart.png')
+            .pause(500)
+            .setValue('input[name="session[username_or_email]"]', 'boyishgiggles11')
+            .pause(500)
+            .setValue('input[name="session[password]"]', 'DOMEGALULC')
+            .pause(500)
+
+        .click('input[value="Log in"]')
+            .moveToElement('input[value="Log in"]', 0, 0)
+            .mouseButtonClick(0)
+            //.assert.containsText('DisRespect')
+            .click('div[name="tweet"]')
+            .moveToElement('div[name="tweet"]', 0, 0)
+            .mouseButtonClick(0)
+            .pause(500)
+            .setValue('div[name="tweet"]', 'D OMEGALUL C')
+            .pause(500)
+            .click('button[class="tweet-action EdgeButton EdgeButton--primary js-tweet-btn"]', browser.Keys.ENTER)
+            .saveScreenshot(config.imgpath(browser) + 'logindata.png')
+            .pause(1000)
+            .end();
+    }
 };
