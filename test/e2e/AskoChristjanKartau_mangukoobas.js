45325fe013ca5ebedf90331c81694f24d29f8877
Asko <asko-christjan.kartau@tptlive.ee>
2018-10-02 15:25:23 +0300

    Jaanus

--- test/e2e/AskoChristjanKartau_mangukoobas.js
+++ test/e2e/AskoChristjanKartau_mangukoobas.js
@@ -5,6 +5,7 @@ module.exports = { // addapted from: https://git.io/vodU0
   'Open google': function (browser) {
     // load google
     browser
+      .resizeWindow(1920, 1080)
       .url('http://mangukoobas.delfi.ee/')
       .pause(1000)
       .waitForElementVisible('body')
@@ -13,6 +14,7 @@ module.exports = { // addapted from: https://git.io/vodU0
       .pause(1000)
       .click('a[href="/id/5/action/themes/channel/4/"]')
       .pause(1000)
+      .click('[class="btn btn_red btn1"]')
       .end();
   },
 };
