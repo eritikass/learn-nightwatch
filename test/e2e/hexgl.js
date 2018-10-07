eafed87063c698f4dfa02db535201d00f9c2fd84
Gert-Andry <gert-andry.kaaramees@tptlive.ee>
2018-10-07 21:41:03 +0300

    at least i tried

---
+++ test/e2e/hexgl.js
@@ -0,0 +1,29 @@
+//http://hexgl.bkcore.com/play/
+const config = require('../../nightwatch.conf.js');
+
+module.exports = { // addapted from: https://git.io/vodU0
+  '@tags': ['google'],
+  'Open mario': function (browser) {
+    // Open mario
+    browser
+    .resizeWindow(1920, 1080)
+    .url('http://hexgl.bkcore.com/play/')
+      .pause(1000)
+      .waitForElementVisible('body')
+      .click('#start')
+      .pause(2000)
+      .click('#step-2')
+      .pause(5000)
+      for (var i = 0; i < 2000; i++) {
+        browser.sendKeys('body', 'KEY_UP')
+      }
+      browser.end()
+    //  var i = 0;
+    //  do {
+    //    browser.sendKeys('body', 'KEY_RIGHT')
+    //    i++;
+    //}
+    //while(i < 2000)
+    //  browser.end()
+  },
+};
