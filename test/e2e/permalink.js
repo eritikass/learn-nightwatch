21380ac302bdf468d364451a5cb76e6f4a82178e
Tehnikumi Opilane <opilane@tptlive.ee>
2018-10-15 17:17:22 +0300

    done

--- test/e2e/permalink.js
+++ test/e2e/permalink.js
@@ -5,13 +5,16 @@ module.exports = { // addapted from: https://git.io/vodU0
   'Open lastekas': function (browser) {
     // load lastekas
     browser
+      .resizeWindow(1920, 1080)
       .url('https://github.com/eritikass/tpt-vue-test-1')
       .pause(100)
       .click('a[href="/eritikass/tpt-vue-test-1/blob/master/babel.config.js"]')
-      .pause(2000)
+      .pause(1000)
       .keys('y')
-      .pause(2000)
+      .pause(1000)
       .click('#L3')
+      .pause(1000)
+      .execute("alert(window.location.href)")
       .pause(1000)
       .end();
   },
