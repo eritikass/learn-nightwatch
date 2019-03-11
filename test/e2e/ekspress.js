882c44a1b8054a9767435bda8276171b93e3fca6
Hindrek Hannus <hindrek.hannus@tptlive.ee>
2019-03-11 13:40:45 +0200

    Add second test

--- test/e2e/ekspress.js
+++ test/e2e/ekspress.js
@@ -13,8 +13,21 @@ module.exports = { // addapted from: https://git.io/vodU0
       .saveScreenshot(`${config.imgpath(browser)}Ekspressfrontpage.png`)
       .waitForElementVisible('body')
       .pause(1000)
-      .mouseButtonClick(0)
       .saveScreenshot(`${config.imgpath(browser)}EkspressCase1.png`)
       .end();
-  }
+  },
+  'Ekspress, case 2': function (browser) {
+    browser
+      .url('http://ekspress.delfi.ee')
+      .pause(1000)
+      .waitForElementVisible('body')
+      .pause(1000)
+      .setValue('input[class="header-search__input"]', "kits")
+      .click('button[class="header-search__button icon icon--search"')
+      .pause(1000)
+      .waitForElementVisible('body')
+      .saveScreenshot(`${config.imgpath(browser)}Ekspresssearch.png`)
+      .pause(1000)
+      .end();
+  },
 };
