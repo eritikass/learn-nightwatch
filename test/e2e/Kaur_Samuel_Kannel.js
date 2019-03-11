a70198630ef34a91d0ec9db75f8f99126b9ab578
Tehnikumi Opilane <opilane@tptlive.ee>
2019-03-11 14:50:44 +0200

    tunnitöö

--- test/e2e/Kaur_Samuel_Kannel.js
+++ test/e2e/Kaur_Samuel_Kannel.js
@@ -2,12 +2,20 @@ const config = require('../../nightwatch.conf.js');
 
 module.exports = { // addapted from: https://git.io/vodU0
   '@tags': ['kertmoorbach'],
-  'Open kertmoorbach': function (browser) {
+  'Open Eesti ekspress': function (browser) {
     // load google
     browser
       .resizeWindow(1920, 1080)
       .url('https://ekspress.delfi.ee/')
       .waitForElementVisible('body')
+      .pause(2000)
+      .saveScreenshot(`${config.imgpath(browser)}frontpage.png`)
+      .click('a[href= "/kuum/veerpalu-voimalik-kaitsja-voitis-eelmisel-aastal-uliraske-kohtuasja?id=85567893"][class="headline__media"]')
+      .waitForElementVisible('body')
+      .pause(2000)
+      .saveScreenshot(`${config.imgpath(browser)}artikel.png`)
+      .moveToElement('a[href="#"][class="article__comments"]',10,10)
+      .pause(2000)
       .end();
 
       
