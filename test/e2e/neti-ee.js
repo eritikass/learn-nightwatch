6f31f7e811b59644e696bb7977d655c26ec86e8f
Kristin <Kristin@KRISTIN-PC>
2020-04-04 12:11:43 +0300

    update neti.ee test

--- test/e2e/neti-ee.js
+++ test/e2e/neti-ee.js
@@ -1,20 +1,3 @@
-module.exports = {
-  "Test for neti": function (browser) {
-    browser
-      .url("https://www.neti.ee/")
-      .waitForElementVisible("body", 2000) // css
-      .assert.containsText('body', 'Riik ja Ühiskond') // css
-      .useXpath()
-      .assert.containsText("//a[@href='/cgi-bin/teema/RIIK_JA_YHISKOND/Valitsus/']", 'Valitsus') // xpath
-      .saveScreenshot(`${config.imgpath(browser)}neti-ee.png`)
-      .click("//a[@href='/cgi-bin/teema/RIIK_JA_YHISKOND/Valitsus/']") // xpath
-      .useCss()
-      .waitForElementVisible("body", 500) // css
-      .assert.containsText('body', 'Ametlikud Teadaanded (AT)') // css
-      .saveScreenshot(`${config.imgpath(browser)}neti-ee-valitsus.png`)
-      .end();
-  },
-};
 const config = require("../../nightwatch.conf.js");
 
 module.exports = {
