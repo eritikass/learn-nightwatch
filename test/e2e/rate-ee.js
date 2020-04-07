dea5d9d5bd361d4270444351de217075d8ecfc83
Katrin <Katrin.kalberg@tptlive.ee>
2020-04-07 21:17:10 +0300

    Add tests for neti & rate browser

---
+++ test/e2e/rate-ee.js
@@ -0,0 +1,15 @@
+const config = require('../../nightwatch.conf.js');
+module.exports = {
+    "Rate->kirjakast": function (browser) {
+      browser
+        .url("https://www.rate.ee/login")
+        .waitForElementVisible("body", 2000) // css
+        .click('a.icn-messages')
+        //.assert.containsText("body","a.icn-messages") // css
+        .waitForElementVisible("body", 2000) // css
+        .assert.containsText("body", "Seda lehte näevad ainult registreeritud kasutajad.") // css
+        .saveScreenshot(`${config.imgpath(browser)}rate-ee.png`)
+        .end();
+    },
+     
+  };
