4dda6a88f078672da4e8c58e1e2bc31656263a83
Elvis Takkis <elvis.takkis@tptlive.ee>
2020-04-12 13:33:49 +0300

    taltech new Update

--- test/e2e/taltech-ee.js
+++ test/e2e/taltech-ee.js
@@ -1,14 +1,14 @@
 const config = require("../../nightwatch.conf.js");
 
 module.exports = {
-  "Test for Telia": function (browser) {
+  "Test for Taltech": function (browser) {
     browser
       .url("https://taltech.ee/")
       .waitForElementVisible("body", 2000)
       .click('a.xtouch-dbl-clk')
       .useCss()
-      .waitForElementVisible("body", 500)
-      .assert.containsText("body", "Siin on vajaliku infot sisseastujale.")
+      .waitForElementVisible("body", 2000)
+      .assert.containsText("body", "Sisseastuja")
       .saveScreenshot(`${config.imgpath(browser)}taltech-ee-sisseastujale.png`)
       .end();
   },
