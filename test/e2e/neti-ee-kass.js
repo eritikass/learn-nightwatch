702e55e7d8689d800cf80a95f71aef31599c48bf
Liina Adamson <liina.adamson@tptlive.ee>
2020-04-18 00:02:20 +0300

    Fix neti-ee-kass test

--- test/e2e/neti-ee-kass.js
+++ test/e2e/neti-ee-kass.js
@@ -12,8 +12,11 @@ module.exports = {
       .useXpath()
       .assert.containsText("//a[@href='/cgi-bin/teema/MEELELAHUTUS_JA_HOBID/Koduloomad/']", "Koduloomad") // xpath
       .click("//a[@href='/cgi-bin/teema/MEELELAHUTUS_JA_HOBID/Koduloomad/']") // xpath
-      .click("//a[@href='http://loomakaitse.eu/']") //xpath
+      .click("//a[@href='http://loomakaitse.eu/']") //
+      .useCss()
+      .waitForElementVisible("body", 2000) // css
       .assert.urlContains("loomakaitse.eu")
+      .waitForElementVisible("body", 2000) // css
       .saveScreenshot(`${config.imgpath(browser)}loomakaitse.png`)
       .end();
   },
