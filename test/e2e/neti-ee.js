c250f9f6088d7dcead25838b3d73e8ae21952054
Andres-Sikk <andres.sikk@tptlive.ee>
2020-04-24 13:58:59 +0300

    Merge files with same domain name

--- test/e2e/neti-ee.js
+++ test/e2e/neti-ee.js
@@ -23,4 +23,31 @@ module.exports = {
       .assert.containsText("body", "Meelelahutus ja Hobid") // css
       .end();
   },
+  /*
+ * Open https://www.neti.ee/
+ * Search "kass"
+ * Check that page contains link to "Koduloomad"
+ * Open "Koduloomad"
+ * Open "Eestimaa Loomakaitse Liit"
+ * Check url contains "loomakaitse.eu"
+ * Take screenshot
+*/
+  "Kass -> Koduloomad -> Eestimaa Loomakaitse Liit -> loomakaitse.eu": function (browser) {
+    browser
+      .useXpath()
+      .resizeWindow(1200, 800)
+      .url("https://www.neti.ee/")
+      .waitForElementVisible("html/body", 2000)
+      .setValue('//input[@type="text"]', "kass")
+      .click('//input[@type="submit"]')
+      .waitForElementVisible("html/body", 500)
+      .assert.visible('//a[@href="/cgi-bin/teema/MEELELAHUTUS_JA_HOBID/Koduloomad/"]/span[text()="Koduloomad"]')
+      .click('//a[@href="/cgi-bin/teema/MEELELAHUTUS_JA_HOBID/Koduloomad/"]/span[text()="Koduloomad"]')
+      .waitForElementVisible("html/body", 500)
+      .click("//a[text()='Eestimaa Loomakaitse Liit']")
+      .waitForElementVisible("html/body", 500)
+      .expect.url().to.contain("loomakaitse.eu");
+    browser.saveScreenshot(`${config.imgpath(browser)}loomakaitse.png`)
+      .end();
+  },
 };
