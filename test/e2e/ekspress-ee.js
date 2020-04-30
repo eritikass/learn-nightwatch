43a27e51537fa96aa62df3113a587c46eee8130e
tonn24 <tonn.toomsalu@gmail.com>
2020-04-30 14:29:46 +0300

    Ekspress test is working

--- test/e2e/ekspress-ee.js
+++ test/e2e/ekspress-ee.js
@@ -1,53 +1,59 @@
-const config = require("../../nightwatch.conf.js");
+const conf = require("../../nightwatch.conf.js");
 
 module.exports = {
-  "Ekspress": function(browser) {
+  "Ekspress.ee": function (browser) {
     browser
-    .url("https://ekspress.delfi.ee/")
-    .waitForElementVisible('body', 3000)
-    .useXpath()
-    .click("//*[@id='header']/div[1]/div[2]/div[2]/div[1]/ul/li[5]/a")
-    .click("//*[@id='header']/div[1]/ul/li[3]/a")
-    .useCss()
-    .pause("2000")
-    .useXpath()
-    .click("//*[@id='main']/section/div[1]/div[1]/ul/li[5]/a")
-    .useCss()
-    .pause("2000")
-    .useXpath()
-    .click("//*[@id='main']/section/div[1]/div[1]/ul/li[5]/a")
-    .useCss()
-    .pause("2000")
-    .useXpath()
-    .waitForElementVisible("//*[@id='main']/section/div[2]/div[1]/div/div[1]/div/a/figure/div/img")
-    .click("//*[@id='main']/section/div[2]/div[1]/div/div[1]/div/div/a[2]")
-    .useCss()
-    .pause("5000")
-    .useXpath()
-    .assert.elementPresent("//*[@id='article-85017801']/div[1]/div/div/div[1]/div[1]/div[3]/div/div[2]/ul/li[3]/a", "Kuula nupp")
-    .moveToElement("//*[@id='article-85017801']/div[2]/div/div[1]/div/div[1]/div[1]/a/figure/div/img", 9, 9)
-    .pause("4000")
-    .saveScreenshot(`${config.imgpath(browser)}seotud-lood.png`)
-    .pause("1000")
-    .click("//*[@id='article-85017801']/div[2]/div/div[1]/div/div[1]/div[1]/div/h1/a[1]")
-    .pause("2000")
-    .assert.elementPresent("//*[@id='article-89550773']/div[1]/div/div/div[1]/div[1]/div[3]/div/div[2]/ul/li[3]/a", "Kuula nupp")
-    .pause("2000")
-    .assert.elementPresent("//*[@id='article-89550773']/div[1]/div/div/div[1]/div[1]/div[3]/div/div[1]/div/a/div[2]/div[1]", "Autor")
-    .saveScreenshot(`${config.imgpath(browser)}artikkel.png`)
-    .end()
-  }
-}
+      .setWindowSize(1200, 1200)
+      .url("https://ekspress.delfi.ee/")
+      .waitForElementVisible("body")
+      .useXpath()
+      .click(
+        '//li[@class="navigation-top__item navigation-top__item--areen"]/a[text()="Areen"]'
+      )
+      .waitForElementVisible(
+        '//li[@class="header__navigation-bottom__item"]/a[text()="Kirjandus"]'
+      )
+      .click(
+        '//li[@class="header__navigation-bottom__item"]/a[text()="Kirjandus"]'
+      )
+      .waitForElementVisible("//body");
 
+    // move to 2nd page
+    browser
+      .waitForElementVisible(
+        '(//div[@class="pager-wrapper"]//*[text()="›"])[1]'
+      )
+      .click('(//div[@class="pager-wrapper"]//*[text()="›"])[1]')
+      .waitForElementVisible(
+        "//div[@class='pager-wrapper']//a[@class='pager__button' and contains(text(),'3')]"
+      )
+      .assert.urlContains("teema/kirjandus?page=2");
 
-/* * Open "https://ekspress.delfi.ee/"
- * open "Areen" -> "Kirjandus"
- * move to 3'rd page 
- * open last article from listing
- * Check that article has "kuula" button
- * Scroll to "Seotud lood" section
- * Take screenshot
- * Open 1'st article
- * Check that article has "kuula" button
- * Check that article has author
- * Take screenshot  */
+    // move to 3rd page
+    browser
+      .waitForElementVisible(
+        '(//div[@class="pager-wrapper"]//*[text()="›"])[1]'
+      )
+      .click('(//div[@class="pager-wrapper"]//*[text()="›"])[1]')
+      .waitForElementVisible(
+        "//div[@class='pager-wrapper']//a[@class='pager__button' and contains(text(),'4')]"
+      )
+      .assert.urlContains("teema/kirjandus?page=3");
+
+    // move to the end of articles list
+    browser
+      .waitForElementVisible("(//span[text()='Loe edasi'])[last()]")
+      .moveToElement("(//span[text()='Loe edasi'])[last()]", 0, 0)
+      .pause(2000)
+      .saveScreenshot(`${conf.imgpath(browser)}ekspress-last3.png`);
+
+    // click to last article
+    browser
+      .click("(//span[text()='Loe edasi'])[last()]")
+      .waitForElementVisible('//a[@class="article__listen"]')
+      .assert.containsText('//a[@class="article__listen"]', "KUULA")
+      .saveScreenshot(`${conf.imgpath(browser)}ekspress-article-open.png`);
+
+    browser.pause(3000).end();
+  },
+};
