cf64fbddfbedecfb508140cd9e4f94a027535360
AarneA <aaa7.tpt@gmail.com>
2020-06-05 12:43:58 +0300

    'example.com-test'

---
+++ test/e2e/4_example-com-test.js
@@ -0,0 +1,19 @@
+const config = require ("../../nightwatch.conf.js");
+
+module.exports = {
+"example.com-test": function (browser) {
+    browser
+        .url('http:www.example.com')
+        .waitForElementVisible('body', 3000)
+        .assert.containsText("body","Example Domain")
+        .assert.containsText("a[href='https://www.iana.org/domains/example']", 'More information...')
+        .saveScreenshot(`${config.imgpath(browser)}example-com-more-inf.png`)
+        .click("a[href='https://www.iana.org/domains/example']")
+        .waitForElementVisible("body")
+        .pause(3000)
+        .assert.title('IANA — IANA-managed Reserved Domains')
+        .saveScreenshot(`${config.imgpath(browser)}example-com-IANA-org.png`)
+        .end();
+
+    },
+};
