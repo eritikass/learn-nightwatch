9e865257fb07bb4526b44ed95d649018fb999d07
Kenert Karu <karukenert@gmail.com>
2020-06-11 13:10:43 +0300

    homework done

--- test/e2e/nightwatchjs.js
+++
@@ -1,34 +0,0 @@
-const config = require('../../nightwatch.conf.js');
-
-module.exports = {
-  'Test for nightwatchjs.org': function (browser) {
-    browser
-      .url('https://www.nightwatchjs.org/')
-      .waitForElementVisible('body')
-      .saveScreenshot(`${config.imgpath(browser)}nightwatchjs-org.png`)
-      .resizeWindow(1200, 800)
-      .saveScreenshot(`${config.imgpath(browser)}nightwatchjs-org.png`)
-      .useXpath()
-      .click('//div[@id="navbar"]//a[text()="API Reference"]')
-      .useCss()
-      .waitForElementVisible('body', 1500)
-      .saveScreenshot(`${config.imgpath(browser)}nightwatchjs-org.png`);
-
-    browser.pause(2000).end();
-  },
-};
-
-
-// browser       .url("https://rate.ee/register")       
-// .waitForElementVisible("body")       
-// .resizeWindow(1200, 800)       
-// .useXpath()       
-// //   .click('//a[@href="/kasutatud/kasutatud.php"]')       
-// .click("//a[@id='fake-city']")       
-// .useCss()       
-// .waitForElementVisible('#modal-city-name')       
-// .setValue('#modal-city-name', 'tallinn')       
-// .useXpath()       
-// .waitForElementVisible('//button[text()="Tallinn (Harju maakond)"]')       
-// .click('//button[text()="Tallinn (Harju maakond)"]')       
-// .pause(2000); 
